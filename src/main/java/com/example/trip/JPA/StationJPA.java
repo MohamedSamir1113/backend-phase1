ff7e73e09955c1f00675c6bf3b0c56f1650cba00
Samir1113 <simo2000854@gmail.com>
2022-11-11 21:32:43 +0200

    changes in station jpa

--- src/main/java/com/example/trip/JPA/StationJPA.java
+++ src/main/java/com/example/trip/JPA/StationJPA.java
@@ -1,5 +1,5 @@
 package com.example.trip.JPA;
-import com.example.demo.Entities.Station;
+import com.example.trip.Entities.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface StationJPA extends JpaRepository <Station , Long> {
 
