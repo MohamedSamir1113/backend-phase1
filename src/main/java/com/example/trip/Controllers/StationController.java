acff3cc67728a8b0224bda2668fc316011879f39
MahmoudBadr205 <93834665+MahmoudBadr205@users.noreply.github.com>
2022-11-11 21:59:20 +0200

    added ststion controller

--- src/main/java/com/example/trip/Controllers/StationController.java
+++ src/main/java/com/example/trip/Controllers/StationController.java
@@ -1,4 +1,42 @@
 package com.example.trip.Controllers;
 
+import com.example.trip.Entities.Station;
+import com.example.trip.Services.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/station")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StationController {
+    @Autowired
+    StationService stationservice;
+
+    @PostMapping("/create")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void createStation(@RequestBody Station s){
+        stationservice.createStation(s);
+    }
+
+    @GetMapping("/get/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Station getStationById(@PathVariable Long id){
+        return stationservice.getStation(id);
+
+    }
+
+    @GetMapping("/getall")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<Station> getAllStations(){
+        return stationservice.getAllStations();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void deleteStation(@PathVariable Long id){
+        stationservice.deleteStation(id);
+    }
 }
