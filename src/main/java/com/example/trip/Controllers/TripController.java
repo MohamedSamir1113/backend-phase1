45e3c2bcef8fbef221b26b39a115a595fd7cce27
MahmoudBadr205 <93834665+MahmoudBadr205@users.noreply.github.com>
2022-11-11 22:09:19 +0200

    implment trip controller

--- src/main/java/com/example/trip/Controllers/TripController.java
+++ src/main/java/com/example/trip/Controllers/TripController.java
@@ -1,5 +1,48 @@
 package com.example.trip.Controllers;
+import com.example.trip.Entities.Trip;
+import com.example.trip.Services.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/trip")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TripController {
+    @Autowired
+    TripService tripService;
+
+    @PostMapping("/createTrip")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void createTrip(@RequestBody Trip t){
+        tripService.createTrip(t);
+
+    }
+
+    @GetMapping("/getTrip/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Trip getTripById(@PathVariable Long id)
+    {
+        return tripService.getTrip(id);
+    }
+
+    @GetMapping("/getAll")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<Trip> getAllTrips(){
+        return tripService.getAllTrips();
+    }
+
+    @DeleteMapping("/deleteTrip/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void deleteTrip(@PathVariable Long id){
+        tripService.deleteTrip(id);
+    }
+
+    @PutMapping("/updateTrip/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Trip updateTrip(@RequestBody Trip body ,@PathVariable Long id){
+        return tripService.updateTrip(body, id);
+    }
 
 }
